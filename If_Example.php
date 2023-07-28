f25ef95c7453eb8205a52f6f361c66edb02159a3
sohailkarmani <sohailkarmani16@gmail.com>
2023-07-28 09:11:17 +0500

    file edit

--- If_Example.php
+++ If_Example.php
@@ -1,9 +1,20 @@
 <?php
 $a=4;
-$b=5;
+$b=4;
 if($a==$b){
-    echo "Values are equal";
+    echo "Values are equal <br>";
 
 }
+echo "value are not equal<br>";
+?>
+
+
+<?php
+$x=5;
+$y=6;
+if($x==$y):
+    echo "Equal ";
+endif;
 echo "value are not equal";
+
 ?>
