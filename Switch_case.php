fc393c9944ea4411cffb1e436e49249989106f7a
sohailkarmani <sohailkarmani16@gmail.com>
2023-07-29 10:20:01 +0500

    fiel is added

---
+++ Switch_case.php
@@ -0,0 +1,29 @@
+<?php
+$weekDay=6;
+switch($weekDay){
+    case 1:
+        echo "Today is Monday";
+        break;
+    case 2:
+        echo "Today is Tuesday";
+        break;    
+    case 3:
+        echo "Today is Wenday";
+        break;
+    case 4:
+        echo "Today is Thrusday";
+        break;
+    case 5:
+        echo "Today is Friday";
+        break;
+    case 6:
+        echo "Today is Saturday";
+        break;
+    case 7:
+        echo "Today is Sunday";
+        break;
+    default:
+        echo "pls enter valid number";
+                                
+}
+?>
